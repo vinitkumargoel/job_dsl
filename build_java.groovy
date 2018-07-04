207fae0dc576242380e4d4619f760973bb1da69e
vinit.kumar1 <vinit.kumar7@wipro.com>
2018-07-04 19:09:31 +0530

    update jenkin file

--- build_java.groovy
+++ build_java.groovy
@@ -1,6 +1,9 @@
 job("build-java") {
+    parameters{
+      string(name: 'branch', defaultValue: 'master', description: 'Please enter branch')
+    } 
     scm{
-        git("https://github.com/vinitkumargoel/jenkin_l1")
+        git("https://github.com/vinitkumargoel/jenkin_l1","master")
     }
     steps {
         shell('javac JavaProgram.java')
