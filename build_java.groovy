429dbf915275fc6088e4a555e2c65a03b492cee2
vinit.kumar1 <vinit.kumar7@wipro.com>
2018-07-04 19:25:57 +0530

    aaa

--- build_java.groovy
+++ build_java.groovy
@@ -1,3 +1,11 @@
+def pipelineScript = 'your pipeline script'
+
+def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
+
+scriptApproval.approveScript(scriptApproval.hash(pipelineScript, 'groovy'))
+
+script pipelineScript
+
 job("build-java") {
     parameters{
       string(name: 'branch', defaultValue: 'master', description: 'Please enter branch')
