3dd7d2089c053e0041af2f6ac270578f1945723e
vinit.kumar1 <vinit.kumar7@wipro.com>
2018-07-05 16:03:16 +0530

    ass

--- build_java.groovy
+++ build_java.groovy
@@ -1,7 +1,4 @@
 job("build-java") {
-    parameters{
-      string(name: 'branch', defaultValue: 'master', description: 'Please enter branch')
-    } 
     scm{
         git("https://github.com/vinitkumargoel/jenkin_l1","master")
     }
